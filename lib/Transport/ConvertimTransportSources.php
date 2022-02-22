308e1454f3952f037fabca21080b8bc91b132c85
Tomas Pesek <tomas.pesek@shopsys.com>
2022-02-22 08:37:28 +0100

    added supported transport sources types

---
+++ lib/Transport/ConvertimTransportSources.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Convertim\Transport;
+
+class ConvertimTransportSources
+{
+    const SOURCE_PPL = 'ppl';
+    const SOURCE_DPD = 'dpd';
+    const SOURCE_PACKETA = 'packeta';
+    const SOURCE_SK_DPD = 'sk-dpd';
+    const SOURCE_STORES = 'stores';
+}
