ddf861c34ef66b84b4aa092b395d0adf07e67261
Tomas Pesek <pesovo@gmail.com>
2022-06-01 16:13:44 +0200

    fixed ConvertimOrderMapper

--- lib/Order/ConvertimOrderDataFactory.php
+++ lib/Order/ConvertimOrderDataFactory.php
@@ -38,12 +38,12 @@ class ConvertimOrderDataFactory
         }
 
         $registerToNewsletter = false;
-        if (array_key_exists('registerToNewsletter', $orderJsonArray) && $orderJsonArray['header']['registerToNewsletter'] !== null) {
+        if (array_key_exists('registerToNewsletter', $orderJsonArray['header']) && $orderJsonArray['header']['registerToNewsletter'] !== null) {
             $registerToNewsletter = $orderJsonArray['registerToNewsletter'];
         }
 
         $registerToEshop = false;
-        if (array_key_exists('registerToEshop', $orderJsonArray) && $orderJsonArray['header']['registerToEshop'] !== null) {
+        if (array_key_exists('registerToEshop', $orderJsonArray['header']) && $orderJsonArray['header']['registerToEshop'] !== null) {
             $registerToEshop = $orderJsonArray['registerToEshop'];
         }
 
