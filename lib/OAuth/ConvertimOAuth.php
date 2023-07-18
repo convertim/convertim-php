8b5b70667d90d1d89eb22d0ea107676b4b24061e
Tomas Pesek <pesovo@gmail.com>
2023-07-19 00:20:43 +0200

    added parse and validation token

--- lib/OAuth/ConvertimOAuth.php
+++ lib/OAuth/ConvertimOAuth.php
@@ -4,6 +4,14 @@ namespace Convertim\OAuth;
 
 use Convertim\OAuth\Exceptions\ConvertimOauthBatResponseException;
 use GuzzleHttp\Client;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Signer\Key\LocalFileReference;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token\Parser;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\JWT\Validation\Validator;
 
 class ConvertimOAuth
 {
@@ -30,17 +38,31 @@ class ConvertimOAuth
         $this->options = $options;
     }
 
+    /**
+     * @param string $authCode
+     * @return \Lcobucci\JWT\Token
+     */
     public function getUserLoginToken($authCode)
     {
         $apiResponse = $this->getApiClient()->doRequest($this->options['clientId'], $this->options['clientSecret'], $authCode);
 
         try {
             $apiResponseJson = json_decode($apiResponse->getBody(), true);
-            if (isset($apiResponseJson['token'])) {
-                return $apiResponseJson['token'];
+            if (!isset($apiResponseJson['token'])) {
+                throw new ConvertimOauthBatResponseException('Token was not return in response', 0);
             }
 
-            throw new ConvertimOauthBatResponseException('Token was not return in response', 0);
+            $parser = new Parser(new JoseEncoder());
+            $validator = new Validator();
+
+            $stringToken = $apiResponseJson['token'];
+            $token = $parser->parse($stringToken);
+            if (! $validator->validate($token, new SignedWith(new Sha256(), InMemory::file('./assets/convertim_oauth.pub')))) {
+                throw new ConvertimOauthBatResponseException('Token is invalid', 0);
+            }
+
+            return $token;
+
         } catch (\Exception $exception) {
             throw new ConvertimOauthBatResponseException($exception->getMessage(), $exception->getCode(), $exception);
         }
